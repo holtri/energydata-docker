a3c2668f3fda23347fcd743a3baec944959ce062
Michael Vollmer <MRVollmer@users.noreply.github.com>
2017-09-13 13:52:36 +0200

    move the plugin install to a more logical position

--- stream-simulation/startup.sh
+++ stream-simulation/startup.sh
@@ -6,10 +6,10 @@ docker exec -t -u 0 energydatadocker_broker_1 kafka-topics --zookeeper zookeeper
 docker cp ./stream.cql energydatadocker_node1_1:/
 docker exec -t -u 0 energydatadocker_node1_1 cqlsh -e "SOURCE 'stream.cql'"
 
-docker exec -t energydatadocker_jupyter_1 pip install kafka-python
 docker cp ./cassandraConnector.properties energydatadocker_broker_1:/
 docker exec -t -u 0 energydatadocker_broker_1 nohup ./usr/bin/connect-standalone ./etc/kafka/connect-standalone.properties ./cassandraConnector.properties &
 
+docker exec -t energydatadocker_jupyter_1 pip install kafka-python
 docker cp ./data/ energydatadocker_jupyter_1:/home/jovyan/
 docker exec -t -u 0 energydatadocker_jupyter_1 nohup python /home/jovyan/data/stream.py &
 
